fef314d7c28cf687aa4ee5f716840b2826310d84
Emmanuel Noi <Emmanuel.noi@student.fairfield.edu>
2020-12-19 00:36:05 -0500

    heroku deploy7

--- server.js
+++ server.js
@@ -1,8 +1,10 @@
 //Install express server
 const express = require('express');
+const cors = require('cors');
 const path = require('path');
 const app = express();
 // Serve only the static files form the dist directory
+app.use(cors());
 app.use(express.static(__dirname + '/dist/frontend'));
 app.get('/*', function(req,res) {
   res.sendFile(path.join(__dirname+'/dist/frontend/index.html'));
